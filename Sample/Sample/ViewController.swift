de920b5ae88230305ddcfada7b154b0ec56a81a0
= <=>
2015-10-05 21:49:33 +0900

    [modify]テスト追加

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by 具志堅 雅 on 2015/10/05.
+//  Copyright (c) 2015年 Bloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    Debug.log("ここまできてる")
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
